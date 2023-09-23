03f1b33e5078869c6a6eeb346722f07ab94c4546
Gayatri Dunakhe GD <74578232+GayatriDunakhe@users.noreply.github.com>
2023-09-23 05:38:50 +0000

    Added day22 react projects

---
+++ React/22-9-23/day22-react/src/components/Gayatri.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Gayatri(){
+    return <h1>Hello Gayatri Here!!</h1>;
+}
+
+export default Gayatri;
---
+++ React/22-9-23/day22-react/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home(){
+    // return <h1 className='Home'>Home</h1>;
+    return (
+        <div>
+          <h1>Home</h1>
+          <p>Welcome to the Home page!</p>
+        </div>
+      );
+}
+
+export default Home;
